98abef9388994c9122b23e7bc39cbed4e34203e3
NhungPP <phuongnhungpham8@gmail.com>
2023-04-19 15:46:46 +0700

    work for CMS exercise

--- assets/test-qa.js
+++ assets/test-qa.js
@@ -1,13 +1,19 @@
-class ShowMoreButton extends HTMLElement {
-    constructor() {
-      super();
-      const button = this.querySelector('btn-open-close');
-      button.addEventListener('click', (event) => {
-        const parentDisplay = event.target.closest('quest-anser').closest('.quest-anser');
-      });
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
     }
-  }
-  
-  customElements.define('show-more-button', ShowMoreButton);
-  
-  
+  };
+});
