a47bd52a579c57da8fd79978aa8e08be53eedee5
NhungPP <phuongnhungpham8@gmail.com>
2023-04-25 11:38:01 +0700

    quick view product

--- assets/test-ajax1.js
+++ assets/test-ajax1.js
@@ -1,7 +1,15 @@
 
 function closePopup(){
     document.getElementById("jsclosepoup").addEventListener('click',function(){
-        // alert('hello NPP');
+        alert('You are will close popup');
+        document.getElementById("show-mini-cart").style.display= 'none';
+        document.getElementsByClassName('show-mini-cart').style.display='none';
+    },true);
+}
+
+function loadMiniCart(){
+    document.getElementById("load-cart").addEventListener('click',function(){
+        alert('hello NPP');
         document.getElementById('show-mini-cart').style.display= 'none';
         document.getElementsByClassName('show-mini-cart').style.display='none';
     },true);
